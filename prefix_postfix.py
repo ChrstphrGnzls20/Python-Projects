fce65c384d5f752c846b0deb1442799036def9a6
Christopher Gonzales <cgonzalesmain@gmail.com>
2021-04-01 21:22:22 +0800

    New Project (modified)

--- prefix_postfix.py
+++ prefix_postfix.py
@@ -1,3 +1,5 @@
+# a simple evaluation of prefix and postfix notation
+
 def evaluate(a, b, op):
     if op == '+':
         return int(a) + int(b)
@@ -8,41 +10,54 @@ def evaluate(a, b, op):
     elif op == '/':
         return int(a) / int(b)
 
-def pre_fix(expre):
-    lst = expre.split(' ')
-    stack = []
-    for num in lst[::-1]:
-        if num.isdigit():
-            stack.append(num)
-        else:
-            num2 = stack.pop()
-            num1 = stack.pop()
-            operator = num
-            stack.append(evaluate(num1, num2, operator))
-    else:
-        print('Done evaluating.\n{} = {}'.format(expre,stack.pop()))
-
 def post_fix(expre):
-    lst = expre.split(' ')
+    lst = expre.strip().split(' ')
     stack = []
+    # if the expression is not valid (at min: 2 operands, 1 operator)
+    if len(lst) < 3:
+        return
+
+    # if the given expression and the method is not applicable, simply reverse the list
+    elif not lst[0].isdigit():
+        lst.reverse()
+
+    # if the given expression is valid
     for num in lst:
+        # if it is an operand, append to stack
         if num.isdigit():
             stack.append(num)
         else:
-            num2 = stack.pop()
-            num1 = stack.pop()
-            operator = num
-            stack.append(evaluate(num1, num2, operator))
-    else:
-        print('Done evaluating.\n{} = {}'.format(expre,stack.pop()))
+            # returns None if it detects an error, usually if the expression is not valid
+            try:
+                num2 = stack.pop()
+                num1 = stack.pop()
+                operator = num
+                stack.append(evaluate(num1, num2, operator))
+            except IndexError:
+                return None
+    return stack.pop()
+
+
+def pre_fix(expre):
+    # reverse the string and apply post-fix calculation
+    my_str = expre[::-1]
+    return post_fix(my_str)
 
 def solve():
-    expression: str = input('Enter the expression to be evaluated: ')
+    ans = 0
+    # user prompt
+    expression = input('Enter the expression to be evaluated: ')
     choice = input('Choose from below the method:\n\t1. Pre-fix\n\t2. Post-fix\n\tWhat is your choice?: ')
+
+    # choose what method to apply
     if choice == '1':
-        pre_fix(expression)
+        ans = pre_fix(expression)
     elif choice == '2':
-        post_fix(expression)
+        ans = post_fix(expression)
+
+    # results prompt
+    print(f'Done evaluating.\n{expression.strip()} = {ans}') if ans is not None else print('Invalid expression.')
+
 
 if __name__ == '__main__':
     solve()
